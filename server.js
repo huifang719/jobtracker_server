e7c748b670f515509cc902648fd8b98026888f3d
huifang <hui-f.gu@hotmail.com>
2022-10-25 16:27:42 +1030

    sign up linked with front end

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const dotenv = require('dotenv').config()
 const app = express()
 const PORT = 3001
+
 
 const logger = require('./middlewares/logger')
 const sessions = require('./middlewares/sessions')
