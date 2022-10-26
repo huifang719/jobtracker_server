b919a2437749cd766d686891d1ddc54b72f30bb2
huifang <hui-f.gu@hotmail.com>
2022-10-26 14:17:10 +1030

    deploy for heroku

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const app = express()
-const PORT = 3001
-
+const port = process.env.PORT || 3001;
 
 const logger = require('./middlewares/logger')
 const sessions = require('./middlewares/sessions')
@@ -19,3 +18,12 @@ app.use(express.json())
 app.use(sessions) 
 app.use('/api/users', usersController)
 app.use('/api/sessions', sessionsController) 
+
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path')
+  app.use(express.static(path.join(__dirname, 'build')));
+
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
