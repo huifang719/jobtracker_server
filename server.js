d46bc021d7203279a668fa22f7bbdb7db42a9ee2
huifang <hui-f.gu@hotmail.com>
2022-10-26 16:42:59 +1030

    heroku push

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const dotenv = require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT || 3001;
-
+// const PORT = 3001;
 const logger = require('./middlewares/logger')
 const sessions = require('./middlewares/sessions')
 
