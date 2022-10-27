5e350a9067f1744ef948892c3edf774192d874ab
huifang <hui-f.gu@hotmail.com>
2022-10-27 16:25:51 +1030

    heroku deploy

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt')
 const User = require('../models/user')
 
 router.post('/', (req, res) => {
-  const {name, email, password, confirmPassword} = req.body
+  const {name, email, password} = req.body
   
   const passwordDigest = bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
 
