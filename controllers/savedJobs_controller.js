2feca2732c54f00c3a6b44b0b2fe85273ef30f03
huifang <hui-f.gu@hotmail.com>
2022-11-30 11:55:25 +1030

    update for deploy on render

--- controllers/savedJobs_controller.js
+++ controllers/savedJobs_controller.js
@@ -5,11 +5,12 @@ const SavedJob = require('../models/SavedJob')
 router.post('/', (req, res) => {
   const {title, description, location, url} = req.body[0] 
   const email = req.body[1]
- 
-  SavedJob 
-    .create(title, description,location, url, email) 
+  console.log(title)
+  
+  // SavedJob 
+  //   .create(title, description,location, url, email) 
 
-    .then(res => console.log("saved"))
+  //   .then(res => console.log("saved"))
 })
 
 router.get('/:email', (req, res) => {
