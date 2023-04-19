c576ea0dd1d85fbaafafd1ac9998e0080e793cd5
huifang <hui-f.gu@hotmail.com>
2023-04-19 16:10:30 +0930

    delete unused import.

--- controllers/sessions_controller.ts
+++ controllers/sessions_controller.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { Session, SessionData } from 'express-session';
+import { SessionData } from 'express-session';
 import bcrypt from 'bcrypt';
 import User from '../models/User';
 import UserDto from '../dto/UserDto';
