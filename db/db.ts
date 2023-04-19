850bb4f89ad84db6da21166d1fdcf38c063bf367
huifang <hui-f.gu@hotmail.com>
2023-04-19 16:52:01 +0930

    change db setting for deployment

--- db/db.ts
+++ db/db.ts
@@ -1,8 +1,34 @@
-import pg from 'pg'
+// import pg from 'pg'
 
-const db = new pg.Pool({
-    database: 'job_tracker',
-    password: '1111'
-})
+// const db = new pg.Pool({
+//     database: 'job_tracker',
+//     password: '1111'
+// })
+
+// export default db;
+
+import pg from 'pg'
+const localDbName = 'job_tracker'
+
+let db;
+if (process.env.DATABASE_URL) {
+  db = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  })
+} else {
+  if (process.env.DB_PASSWORD) {
+    db = new pg.Pool({
+      database: localDbName,
+      password: process.env.DB_PASSWORD
+    })
+  } else {
+    db = new pg.Pool({
+      database: localDbName
+    })
+  }
+}
 
 export default db;
