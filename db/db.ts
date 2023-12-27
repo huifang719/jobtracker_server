2c5c98e6cd6b040cd9bf45da4560c5b1afa0e30f
livia <livia.gu@notitia.consulting>
2023-12-27 23:09:01 +1030

    add types in db file

--- db/db.ts
+++ db/db.ts
@@ -7,28 +7,29 @@
 
 // export default db;
 
-import pg from 'pg'
-const localDbName = 'job_tracker'
+import { Pool, PoolConfig } from "pg";
 
-let db;
+const localDbName = "job_tracker";
+let db: Pool;
+
 if (process.env.DATABASE_URL) {
-  db = new pg.Pool({
+  const poolConfig: PoolConfig = {
     connectionString: process.env.DATABASE_URL,
     ssl: {
-      rejectUnauthorized: false
-    }
-  })
-} else {
-  if (process.env.DB_PASSWORD) {
-    db = new pg.Pool({
-      database: localDbName,
-      password: process.env.DB_PASSWORD
-    })
-  } else {
-    db = new pg.Pool({
-      database: localDbName
-    })
-  } 
-}
+      rejectUnauthorized: false,
+    },
+  };
 
-export default db;
+  db = new Pool(poolConfig);
+} else {
+  const poolConfig: PoolConfig = process.env.DB_PASSWORD
+    ? {
+        database: localDbName,
+        password: process.env.DB_PASSWORD,
+      }
+    : {
+        database: localDbName,
+      };
+
+  db = new Pool(poolConfig);
+}
