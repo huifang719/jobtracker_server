c055d24e55d6994bc928fb022d1bf7722e51ba43
livia <livia.gu@notitia.consulting>
2023-12-27 23:06:59 +1030

    add types

--- controllers/sessions_controller.ts
+++ controllers/sessions_controller.ts
@@ -1,56 +1,59 @@
-import express from 'express';
-import { SessionData } from 'express-session';
-import bcrypt from 'bcrypt';
-import User from '../models/User';
-import UserDto from '../dto/UserDto';
+import express, { Request, Response } from "express";
+import { SessionData } from "express-session";
+import bcrypt from "bcrypt";
+import User from "../models/User";
+import UserDto from "../dto/UserDto";
 
-const router = express.Router()
+const router = express.Router();
 
 interface CustomSessionData extends SessionData {
   userId?: number;
 }
-
-router.get('/', (req, res) => {
+interface User {
+  email: string;
+  password_digest: string;
+  name: string;
+  id: number;
+}
+router.get("/", (req: Request, res: Response) => {
   const customSessionData: CustomSessionData = req.session as CustomSessionData;
-    if (customSessionData.userId) {
-        User 
-            .findById(customSessionData.userId)
-            .then(email => res.json(email))
+  if (customSessionData.userId) {
+    User.findById(customSessionData.userId).then((email: string) =>
+      res.json(email)
+    );
+  } else {
+    res.json({ error: "no logged in user" });
+  }
+});
+
+router.post("/", (req: Request, res: Response) => {
+  const { email, password } = req.body as UserDto;
+
+  User.findByEmail(email).then((user: User) => {
+    const isValidPassword = bcrypt.compareSync(password, user.password_digest);
+    if (user && isValidPassword) {
+      const customSessionData: CustomSessionData =
+        req.session as CustomSessionData;
+      customSessionData.userId = user.id;
+      res.json(email);
     } else {
-        res.json({error: 'no logged in user'})
+      res.status(400).send({ error: "Incorrect email or/and password" });
     }
-})
+  });
+});
 
-
-router.post('/', (req, res) => {
-    const {email, password} = req.body as UserDto;
-
-    User
-        .findByEmail(email)
-        .then(user => {
-          const isValidPassword = bcrypt.compareSync(password, user.password_digest)
-            if (user && isValidPassword) {
-              const customSessionData: CustomSessionData = req.session as CustomSessionData;
-                customSessionData.userId = user.id;
-                res.json(email)
-            } else {
-                res.status(400).send({error: "Incorrect email or/and password"}) 
-            }   
-        })
-})
-
-router.delete('/', (req, res) => {
+router.delete("/", (req: Request, res: Response) => {
   const customSessionData: CustomSessionData = req.session as CustomSessionData;
-    if (!customSessionData.userId) {
-      return res.status(400).send({error: "No user logged in"}) 
-    } 
+  if (!customSessionData.userId) {
+    return res.status(400).send({ error: "No user logged in" });
+  }
 
-  req.session.destroy((err) => {
+  req.session.destroy((err: any) => {
     if (err) {
-      return res.status(500).send({ error: 'Error destroying session' });
+      return res.status(500).send({ error: "Error destroying session" });
     }
-    return res.clearCookie('session'); // optional: clears the session cookie from the client
-  })
-})
+    return res.clearCookie("session"); // optional: clears the session cookie from the client
+  });
+});
 
 export default router;
--- controllers/users_controller.ts
+++ controllers/users_controller.ts
@@ -1,26 +1,26 @@
-import express from 'express';
-import bcrypt from 'bcrypt';
-import User from '../models/User';
-import UserDto from '../dto/UserDto';
+import express, { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import User from "../models/User";
+import UserDto from "../dto/UserDto";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/', (req, res) => {
-  const {name, email, password} = req.body as UserDto;
-  
-  const passwordDigest = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+router.post("/", (req: Request, res: Response) => {
+  const { name, email, password } = req.body as UserDto;
 
-  User 
-    .findByEmail(email) 
-    .then((user: UserDto) => {
-      if (user) {
-        res.status(400).send({error: "This email is already associated with an account!" }) 
-      } else {
-        User 
-          .create(name, email, passwordDigest)
-          .then((email:string)=> res.json(email))
-    }   
-  })
-})
+  const passwordDigest = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+
+  User.findByEmail(email).then((user: UserDto) => {
+    if (user) {
+      res
+        .status(400)
+        .send({ error: "This email is already associated with an account!" });
+    } else {
+      User.create(name, email, passwordDigest).then((email: string) =>
+        res.json(email)
+      );
+    }
+  });
+});
 
 export default router;
