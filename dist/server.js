ff148d548cf1e95719d33b1c229af38004fd508c
livia <livia.gu@notitia.consulting>
2024-03-16 09:49:00 +1030

    delete __dir

--- dist/server.js
+++ dist/server.js
@@ -4,8 +4,8 @@ import sessions from "./middleswares/sessions.js";
 import usersController from "./controllers/users_controller.js";
 import sessionsController from "./controllers/sessions_controller.js";
 import savedJobsController from "./controllers/savedJobs_controller.js";
-import path from "path";
-import { __dirname } from "path";
+// import path from "path";
+// import { __dirname } from "path";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -23,9 +23,10 @@ app.use("/api/search", (req, res) => {
 //save jobs
 app.use("/api/save", savedJobsController);
 //for production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-  });
-}
+// console.log(__dirname);
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "build")));
+//   app.get("/*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "build", "index.html"));
+//   });
+// }
