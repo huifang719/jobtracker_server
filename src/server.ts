9f82eb9447691cdc58b2b521b1603c2c9a1a02ac
livia <livia.gu@notitia.consulting>
2024-03-16 10:52:58 +1030

    add cors policy

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,27 @@ import usersController from "./controllers/users_controller.js";
 import sessionsController from "./controllers/sessions_controller.js";
 import savedJobsController from "./controllers/savedJobs_controller.js";
 // import path from "path";
+const whitelist = [
+  "http://localhost:3000",
+  "https://jobtracker-ts.netlify.app/",
+];
+var corsOptions = {
+  origin: (origin: string, callback: any) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error());
+    }
+  },
 
+  exposedHeaders: ["Authorization", "Access-Control-Allow-Origin", "Access"],
+
+  methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "OPTIONS"],
+  preflightContinue: false,
+  //add this header to allow the browser to send the token to the server, possibly solve the cors issue -does not have ok status
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
 const app = express();
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`server listening to port ${PORT}`));
